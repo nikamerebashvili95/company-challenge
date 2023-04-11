1c28c953cd22272f33c558a72e4eb266597704f6
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-04-11 19:36:33 +0400

    update and change javascript functions and array

--- app.js
+++ app.js
@@ -61,39 +61,23 @@ scrollLinks.forEach((link) => {
 const slidereffect = document.querySelectorAll(".news-img");
 const leftslide = document.getElementById("left");
 const rightslide = document.getElementById("right");
-const slidecontent = document.querySelectorAll(".overlay-content");
+
 const savedImg = [
   "./assets/images/slider/img-1.png",
   "./assets/images/slider/img-2.png",
   "./assets/images/slider/img-3.png",
   "./assets/images/slider/img-4.png",
 ];
-const savedText = [
-  { title: "There are many variations of passages", date: "22 April 2023" },
-  { title: "The point of using Lorem Ipsum", date: "20 April 2023" },
-  {
-    title: "I must explain to you how all this mistaken idea",
-    date: "19 April 2023",
-  },
-  {
-    title: "On the other hand, we denounce with righteous indignation",
-    date: "15 April 2023",
-  },
-];
 
 let starter = 1;
+
 function slideright(rightslide) {
   starter += 1;
   for (let i = 0; i < slidereffect.length; i++) {
     slidereffect[i].src = savedImg[(starter + i) % savedImg.length];
   }
-  for (let i = 0; i < slidecontent.length; i++) {
-    const h4 = slidecontent[i].querySelector("h4");
-    const span = slidecontent[i].querySelector("span");
-    h4.textContent = savedText[(starter + i) % savedText.length].title;
-    span.textContent = savedText[(starter + i) % savedText.length].date;
-  }
 }
+
 function slideleft(leftslide) {
   starter -= 1;
   if (starter < 0) {
@@ -101,12 +85,6 @@ function slideleft(leftslide) {
   }
   for (let i = 0; i < slidereffect.length; i++) {
     slidereffect[i].src = savedImg[(starter + i) % savedImg.length];
-  }
-  for (let i = 0; i < slidecontent.length; i++) {
-    const h4 = slidecontent[i].querySelector("h4");
-    const span = slidecontent[i].querySelector("span");
-    h4.textContent = savedText[(starter + i) % savedText.length].title;
-    span.textContent = savedText[(starter + i) % savedText.length].date;
   }
 }
 
